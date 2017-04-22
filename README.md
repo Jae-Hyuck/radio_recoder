83671f1281723eb2601734be0ceb74c31c6c3649
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2017-04-23 04:33:34 +0900

    Fisrt commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # radio_recoder
+FM4U 녹음용 파이썬 스크립트
+
+### Edit crontab
+```sh
+$ crontab -e
+```
+Fro example (새벽 3시부터 3800초)
+```sh
+0 3 * * * python3 /home/jae/extern/Projects/radio_recoder/radio_recoder.py --record_secs 3800 --output_dir /home/jae/Dropbox/MBC_RADIO
+```
+
+### References
+- http://younworld.tistory.com/entry/EBS-%EB%9D%BC%EB%94%94%EC%98%A4-%EC%9E%90%EB%8F%99-%EB%85%B9%EC%9D%8C
+- https://wiki.changwoo.pe.kr/doku.php/project:unattendedradiorecorder
---
+++ radio_recoder.py
@@ -0,0 +1,44 @@
+import json
+from urllib.request import urlopen
+import subprocess
+import datetime
+import argparse
+import os
+import os.path as osp
+
+
+def recording(FLAGS):
+
+    rtmp_addr = json.loads(
+        urlopen('http://miniplay.imbc.com/WebLiveURL.ashx?channel=mfm&agent=&protocol=RTMP')
+        .read().decode('utf-8')[1:-2])['AACLiveURL']
+
+    os.makedirs(FLAGS.output_dir, exist_ok=True)
+    start_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    flv_file = osp.join('/tmp', 'FM4U_' + start_time + '.flv')
+    mp3_file = osp.join(FLAGS.output_dir, 'FM4U_' + start_time + '.aac')
+
+    rtmpdump = ['rtmpdump', '-r', rtmp_addr, '-B', str(FLAGS.record_secs), '-o', flv_file]
+    ffmpeg = ['ffmpeg', '-i', flv_file, '-vn', '-acodec', 'copy', mp3_file]
+    rm = ['rm', flv_file]
+
+    p = subprocess.Popen(rtmpdump)
+    p.wait()
+    p = subprocess.Popen(ffmpeg)
+    p.wait()
+    p = subprocess.Popen(rm)
+    p.wait()
+
+if __name__ == "__main__":
+    ############
+    # Argparse #
+    ############
+    parser = argparse.ArgumentParser()
+
+    # required arguments
+    parser.add_argument('--record_secs', type=int, required=True)
+    parser.add_argument('--output_dir', type=str, required=True)
+
+    FLAGS = parser.parse_args()
+
+    recording(FLAGS)
